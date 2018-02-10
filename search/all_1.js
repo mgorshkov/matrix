be77697390f67a06ff3c39329a86bf2aa2c44236
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-10 04:45:52 +0000

    Deploy mgorshkov/matrix to github.com/mgorshkov/matrix.git:gh-pages

--- search/all_1.js
+++ search/all_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['defaultvalue',['DefaultValue',['../structIndexNode_3_01T_00_010_00_01Default_01_4.html#a68438bd3ea8858503aa5b9e2ffc09b76',1,'IndexNode&lt; T, 0, Default &gt;']]],
-  ['deletenode',['DeleteNode',['../structIExtraNodeDeleter.html#ab9b2788fdd167d0f3cc7a1c02d2ddad1',1,'IExtraNodeDeleter::DeleteNode()'],['../structIndexNode.html#adca6b1b25b0c943e45c576374be3109b',1,'IndexNode::DeleteNode()'],['../structIndexNode_3_01T_00_010_00_01Default_01_4.html#aeb99e82bcd1a751d3ec567749450126d',1,'IndexNode&lt; T, 0, Default &gt;::DeleteNode()']]]
+  ['deletechildnode',['DeleteChildNode',['../structIExtraNodeDeleter.html#ae1a55bbde28f374d327a6ed01189fca9',1,'IExtraNodeDeleter::DeleteChildNode()'],['../structIndexNode.html#a9f562f34bbd2a3bbe7979f0c12e06f92',1,'IndexNode::DeleteChildNode()'],['../structIndexNode_3_01T_00_010_00_01Default_01_4.html#a3f1d79c42affa3f1b1ee3b664f46a2ca',1,'IndexNode&lt; T, 0, Default &gt;::DeleteChildNode()'],['../classMatrix.html#a06e2864630c2050e2ab4192f3360288d',1,'Matrix::DeleteChildNode()']]],
+  ['deletenodefromparentifdefault',['DeleteNodeFromParentIfDefault',['../structIndexNode_3_01T_00_010_00_01Default_01_4.html#a0032b2c22bdd40602872c6cdfd46acb9',1,'IndexNode&lt; T, 0, Default &gt;']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['_7eiextranodedeleter',['~IExtraNodeDeleter',['../structIExtraNodeDeleter.html#a698b6e020e78dc2d10d9ef5904d735c9',1,'IExtraNodeDeleter']]]
+  ['_7eiextranodedeleter',['~IExtraNodeDeleter',['../structIExtraNodeDeleter.html#a698b6e020e78dc2d10d9ef5904d735c9',1,'IExtraNodeDeleter']]],
+  ['_7eindexnode',['~IndexNode',['../structIndexNode.html#ae718be43f75de78ee653f2e86a4fd2c3',1,'IndexNode::~IndexNode()'],['../structIndexNode_3_01T_00_010_00_01Default_01_4.html#ab8a0cd008adc4b874d60df61a8b2b394',1,'IndexNode&lt; T, 0, Default &gt;::~IndexNode()']]]
 ];
